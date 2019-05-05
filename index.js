e6686ee7220ebc30ee8a8c6f91f711b1f248e978
xdagency <pandabear00001@outlook.com>
2019-05-05 13:07:29 -0400

    Getting data from BGG and building the base game object to eventually save to the DB

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const   express = require('express'),
         app = express(),
-        axios = require('axios');
+        axios = require('axios'),
+        parseString = require('xml2js').parseString;
 
 
 // DB
@@ -94,8 +95,99 @@ function checkCurrentDB(min, max, callback) {
 
 }
 
+
+function bgg(id, callback) {
+
+    // Hit the BGG API
+    axios.get('https://www.boardgamegeek.com/xmlapi2/thing?id=' + toScrape[0] + '&stats=1')
+    
+         .then(result => {
+
+            // Initialize parser
+            // let parser = new DOMParser();
+            // Save data/xml from BGG into a variable
+            let xml = result.data;
+            
+            // Empty object to save xml data to
+            let game = {}
+
+            parseString(xml, function(err, result) {
+
+                // First make sure item we are parsing is a BoardGame
+                if (result.items.item[0].$.type !== "boardgame") {
+                    console.log("This is not a boardgame!");
+                    return;
+                }
+
+
+                // Game
+                game = {
+                    name: result.items.item[0].name[0].$.value,
+                    image: result.items.item[0].image[0],
+                    bgg_link: 'https://www.boardgamegeek.com/boardgame/' + toScrape[0] + '/',
+                    game_id: toScrape[0],
+                    min_players: Number(result.items.item[0].minplayers[0].$.value),
+                    max_players: Number(result.items.item[0].maxplayers[0].$.value),
+                    min_time: Number(result.items.item[0].minplaytime[0].$.value),
+                    max_time: Number(result.items.item[0].maxplaytime[0].$.value),
+                    avg_time: Number(result.items.item[0].playingtime[0].$.value),
+                    year: Number(result.items.item[0].yearpublished[0].$.value),
+                    age: 0,
+                    mechanic: '',
+                    category: '',
+                }
+
+
+                // Game age
+                // Check if year published is greater than 0
+                // If it is calculate the age, if not set age to 0
+                game.age = result.items.item[0].yearpublished[0].$.value > 0 ? (new Date().getFullYear() - result.items.item[0].yearpublished[0].$.value) : 0;
+
+
+                // Mechanics and categories
+                let mechanicsAndCategories = result.items.item[0].link;
+
+                // Loop through the categories and mechanics
+                for (let i = 0; i < mechanicsAndCategories.length; i++) {
+                    
+                    // If it's a category
+                    // Save to category string in game object
+                    if (mechanicsAndCategories[i].$.type === "boardgamecategory") {
+                        game.category += mechanicsAndCategories[i].$.value + ', ';
+
+                    // If it's a mechanic
+                    // Save to mechanic string in game object
+                    } else if (mechanicsAndCategories[i].$.type === "boardgamemechanic") {
+                        game.mechanic += mechanicsAndCategories[i].$.value + ', ';
+                    }
+
+                }
+
+
+            })
+            
+            // See what our object looks like
+            console.log(game);
+
+         })
+
+         .then(result => {
+            
+            // After all the data from BGG is grabbed, hit the callback function
+            return callback();
+         })
+
+         .catch(error => {
+             console.log('Hit BGG API Error:', error);
+         })
+
+}
+
+
 checkCurrentDB(minRange, maxRange, () => {
-    process.exit();
+    bgg(1, () => {
+        process.exit();
+    })
 });
 
 // axios.get('https://www.boardgamegeek.com/xmlapi2/thing?id=' + process.argv[2])
